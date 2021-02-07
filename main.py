71c748d06a699a045cb269054f76f2ca4c4fd1cb
Krzysztof Fila <kfila17@gmail.com>
2021-02-07 21:19:46 +0000

    test python bomb

---
+++ main.py
@@ -0,0 +1,9 @@
+# Bomba, ma na celu odlicznaie od 10 do 0, jeśli dochodzi do liczby 2 licznik zwalnia do 3 sekund(jak w filmach XD)
+from time import sleep
+
+for arr in [10,9,8,7,6,5,4,3,2,1,0]:
+    print(arr)
+    sleep(1)
+    if arr <=2:
+        sleep(3)
+print('Boom!')
